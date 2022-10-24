685eb9f2c47b5fa08b52a016c5a6ef57d9fe49f6
Jean-Michel Depaepe <jean-michel.depaepe@equans.com>
2022-10-24 18:01:48 +0200

    exercice

---
+++ src/main/java/com/example/lesson07/VerticalSquare.java
@@ -0,0 +1,41 @@
+package com.example.lesson07;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+
+public class VerticalSquare extends Application {
+    @Override
+    public void start(Stage stage) {
+        Canvas canvas = new Canvas(600, 600);
+        GraphicsContext gc = canvas.getGraphicsContext2D();
+
+        gc.setFill(Color.BLUE);
+        gc.fillRect(0, 0, canvas.getWidth() / 2, canvas.getHeight());
+        gc.setFill(Color.PURPLE);
+        gc.fillRect(canvas.getHeight() / 2, 0, canvas.getWidth(), canvas.getHeight());
+
+        canvas.setOnMouseMoved(event -> {
+            System.out.println(event.getX() + " " + event.getY());
+            if (event.getX() < canvas.getWidth() / 2) {
+                gc.setFill(Color.AQUA);
+                gc.fillRect(0, 0, canvas.getWidth() / 2, canvas.getHeight());
+                gc.setFill(Color.PURPLE);
+                gc.fillRect(canvas.getHeight() / 2, 0, canvas.getWidth(), canvas.getHeight());
+            } else {
+                gc.setFill(Color.BLUE);
+                gc.fillRect(0, 0, canvas.getWidth() / 2, canvas.getHeight());
+                gc.setFill(Color.PINK);
+                gc.fillRect(canvas.getHeight() / 2, 0, canvas.getWidth(), canvas.getHeight());
+            }
+        });
+
+        stage.setScene(new Scene(new VBox(canvas)));
+        stage.setTitle("rectangles placés verticalement");
+        stage.show();
+    }
+}
