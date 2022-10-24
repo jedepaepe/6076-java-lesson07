ea8c2c0dbb7e17b7336219c0af3682c37200e3c8
Jean-Michel Depaepe <jean-michel.depaepe@equans.com>
2022-10-24 18:08:49 +0200

    exercice

---
+++ src/main/java/com/example/lesson07/HorizontalRectangleClick.java
@@ -0,0 +1,41 @@
+package com.example.lesson07;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+
+public class HorizontalRectangleClick extends Application {
+    @Override
+    public void start(Stage stage) {
+        Canvas canvas = new Canvas(600, 600);
+        GraphicsContext gc = canvas.getGraphicsContext2D();
+
+        gc.setFill(Color.BLUE);
+        gc.fillRect(0, 0, canvas.getWidth(), canvas.getHeight() / 2);
+        gc.setFill(Color.PURPLE);
+        gc.fillRect(0, canvas.getHeight() / 2, canvas.getWidth(), canvas.getHeight());
+
+        canvas.setOnMouseClicked(event -> {
+            System.out.println(event.getX() + " " + event.getY());
+            if (event.getY() < canvas.getHeight() / 2) {
+                gc.setFill(Color.AQUA);
+                gc.fillRect(0, 0, canvas.getWidth(), canvas.getHeight() / 2);
+                gc.setFill(Color.PURPLE);
+                gc.fillRect(0, canvas.getHeight() / 2, canvas.getWidth(), canvas.getHeight());
+            } else {
+                gc.setFill(Color.BLUE);
+                gc.fillRect(0, 0, canvas.getWidth(), canvas.getHeight() / 2);
+                gc.setFill(Color.PINK);
+                gc.fillRect(0, canvas.getHeight() / 2, canvas.getWidth(), canvas.getHeight());
+            }
+        });
+
+        stage.setScene(new Scene(new VBox(canvas)));
+        stage.setTitle("demo mouse");
+        stage.show();
+    }
+}
