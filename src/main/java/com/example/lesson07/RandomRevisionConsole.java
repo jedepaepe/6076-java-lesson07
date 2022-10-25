a618088735cde54b298c0c4578aa70852a0833f3
Jean-Michel Depaepe <jean-michel.depaepe@equans.com>
2022-10-25 10:24:53 +0200

    exercice

---
+++ src/main/java/com/example/lesson07/RandomRevisionConsole.java
@@ -0,0 +1,9 @@
+package com.example.lesson07;
+
+public class RandomRevisionConsole {
+    public static void main(String[] args) {
+        for (int i = 0; i < 100; ++i) {
+            System.out.println(Math.random() * 10);
+        }
+    }
+}
